f44a8a28faef3805c89bc363a3fdbf6c37136e7b
Kenneth Reitz <me@kennethreitz.com>
2012-04-28 03:12:23 -0400

    set it up

---
+++ setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+settings = dict()
+
+
+# Publish Helper.
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+settings.update(
+    name='inbox',
+    version='0.0.1',
+    description='SMTP for Humans.',
+    long_description=open('README.rst').read(),
+    author='Kenneth Reitz',
+    author_email='me@kennethreitz.com',
+    url='https://github.com/kennethreitz/inbox.py',
+    modules= ['inbox',],
+    install_requires=['gevent'],
+    license='BSD',
+    classifiers=(
+        # 'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        # 'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    )
+)
+
+
+
+setup(**settings)
